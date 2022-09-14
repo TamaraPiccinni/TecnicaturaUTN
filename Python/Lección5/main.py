d383ce7e37a8bf518746b088883aae450ecfa53a
Tamara Piccinni <tamarapiccinni@gmail.com>
2022-09-13 22:26:14 -0300

    Ingreso los ejercicios del 7 a 9 python

---
+++ Python/Lección5/main.py
@@ -0,0 +1,12 @@
+#Comenzamos con funciones
+
+#mi_funcion() no se puede llamar antes de definirla
+
+#Definimos una función
+# con snake_casse para practicar. los () son necesarios
+
+def mi_funcion(): #para identificar esta funcion usamos ()
+    print('Feliz dia del programador')
+
+mi_funcion()  #llamamos la funcion
+mi_funcion() #se puede llamar N cantidad de veces
