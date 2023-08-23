2f0b480d7002e53ddfe97a631e0a2c452be54edd
Tamara Piccinni <tamarapiccinni@gmail.com>
2023-08-23 19:47:06 -0300

    agrego leccion 4 clase tutores

---
+++ Tecnicatura4Semestre/e-commerce2022/client/js/products.js
@@ -0,0 +1,31 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480, 
+        quanty: 1,
+        img: "https://i.ibb.co/0jZQY0L/banana.jpg"
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: 
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 500,
+        quanty: 1,
+        img: 
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 510,
+        quanty: 1,
+        img: 
+    },
+];
+
